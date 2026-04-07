8675580949520916a5b975ae06e4ff68199bda3f
Stefani Lara Martins MARTINS <u10047649100@acad.pp.ifms.edu.br>
2026-04-07 15:16:16 -0400

    Criamos o bean e dao e a tabela de usuarios no bean

---
+++ src/bean/MpvUsuarios.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bean;
+
+import java.util.Date;
+
+/**
+ *
+ * @author u10047649100
+ */
+public class MpvUsuarios {
+    int     mpvIdUsuarios;
+    String  mpvNome;
+    String  mpvApelido;
+    String  mpvCpf;
+    Date    mpvDataNascimento;
+    int mpvNivel;
+    String mpvSneha;
+    String mpvAtivo;
+}
